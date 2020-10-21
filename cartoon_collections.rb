fd1edd08ec84ff6a860fd8c07fc5c8bf9a12175e
Alexa Botello <abotello20@yahoo.com>
2020-10-21 03:46:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ end
     #   expect(output).to match(/Hello Bashful!/)
     # end
    def greet_characters(dwarves)
-     dwarves = ["dopey", "sneezy", "happy", "angry", "doc", "lemonjello", "sleepy" ]
-oppressed_workers.each do |oppressed_worker|
+     dwarves = ["Dopey", "Grumpy", "Bashful"]
+    dwarves.each do |dwarves|
    puts "#{oppressed_worker.capitalize} wants to start a union!"
 end
     
