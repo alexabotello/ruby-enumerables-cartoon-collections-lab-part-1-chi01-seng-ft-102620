0767db94d13deae2a2e4d605cb4954ff4b911a47
Alexa Botello <abotello20@yahoo.com>
2020-10-21 03:53:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,3 @@ def list_dwarves(array)
   # Print a numbered list of each element
   array.each_with_index { |val,index|
 end
-
-# a = [11,22,31,224,44].each.with_index { |val,index| puts "index: #{index} for #{val}" if val < 30}
-# index: 0 for 11
-# index: 1 for 22
