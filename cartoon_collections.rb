38745e65552595d88d0d688909ff5f740f4dea66
Alexa Botello <abotello20@yahoo.com>
2020-10-21 03:23:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,24 @@
-def greet_characters(array)
+def greet_characters(dwarves)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+output = []
+  do greet_characters.each {|element| print element, "Hello" }
+dwarves
+  end   
 end
+    # output = capture_stdout do
+    #     greet_characters(dwarves)
+    #   end
 
+    #   # This regex allows for arbitrary characters between
+    #   # the numbering and the name
+    #   expect(output).to match(/Hello Dopey!/)
+    #   expect(output).to match(/Hello Grumpy!/)
+    #   expect(output).to match(/Hello Bashful!/)
+    # end
+    
+    
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
