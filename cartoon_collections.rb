9701f8c67bff410ec8b548a5ee3105d0a966dfad
Alexa Botello <abotello20@yahoo.com>
2020-10-21 04:00:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index(1) do | element, index |
-	puts "#{element} is number #{index} in the array"
+	puts "#{element}. #{index}"
 end
 end
