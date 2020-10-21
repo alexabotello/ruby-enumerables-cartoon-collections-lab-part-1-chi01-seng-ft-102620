c65ab99ee2401965a5c975ab65e1fe02bb44a542
Alexa Botello <abotello20@yahoo.com>
2020-10-21 03:48:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,6 @@ end
     #   expect(output).to match(/Hello Grumpy!/)
     #   expect(output).to match(/Hello Bashful!/)
     # end
-   def greet_characters(dwarves)
-    dwarves.each do |dwarves|
-   puts "Hello #{dwarves}!"
-end
     
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
